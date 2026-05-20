f1144d10a4c262579976d8788d6fc1eedd392bdf
swbanga <superwbanga@gmail.com>
2026-05-21 00:00:03 +0200

    Final CSharpFoundation Prac

---
+++ TestProject/Program.cs
@@ -0,0 +1,123 @@
+﻿// Random dice = new Random();
+// int roll = dice.Next(1, 7);
+// Console.WriteLine(roll);
+
+// int firstValue = 500;
+// int secondValue = 600;
+// int largerValue = Math.Max(firstValue, secondValue);
+
+// Console.WriteLine(largerValue);
+
+// Random dice = new Random();
+
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+
+// int total = roll1 + roll2 + roll3;
+
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+
+// Random dice = new Random();
+
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+
+// int total = roll1 + roll2 + roll3;
+
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// {
+//     if ((roll1 == roll2) && (roll2 == roll3))
+//     {
+//         Console.WriteLine("You rolled triples!  +6 bonus to total!");
+//         total += 6;
+//     }
+//     else
+//     {
+//         Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+//         total += 2;
+//     }
+
+//     Console.WriteLine($"Your total including the bonus: {total}");
+// }
+
+// if (total >= 16)
+// {
+//     Console.WriteLine("You win a new car!");
+// }
+// else if (total >= 10)
+// {
+//     Console.WriteLine("You win a new laptop!");
+// }
+// else if (total == 7)
+// {
+//     Console.WriteLine("You win a trip for two!");
+// }
+// else
+// {
+//     Console.WriteLine("You win a kitten!");
+// }
+
+// Random random = new Random();
+// int daysUntilExpiration = random.Next(12);
+// int discountPercentage = 0;
+
+// // Your code goes here
+// if (daysUntilExpiration == 0)
+// {
+//     Console.WriteLine("Your subscription has expired.");
+// }
+// else if (daysUntilExpiration == 1)
+// {
+//     discountPercentage = 20;
+//     Console.WriteLine($"Your subscription expires within a day!\nRenew now and save {discountPercentage}%!");
+// }
+// else if (daysUntilExpiration <= 5)
+// {
+//     discountPercentage = 10;
+//     Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days!\nRenew now and save {discountPercentage}%!");
+// }
+// else if (daysUntilExpiration <= 10)
+// {
+//     discountPercentage = 5;
+//     Console.WriteLine($"Your subscription will expire soon. Renew now!");
+// }
+
+// string[] orders = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+// foreach (string order in orders)
+// {
+//     if (order.StartsWith("B"))
+//     {
+//         Console.WriteLine(order);
+//     }
+// }
+
+// Random coin = new Random();
+// int flip = coin.Next(2);
+// Console.WriteLine((flip == 0) ? "Heads" : "Tails");
+
+// 
+
+// int[] numbers = { 4, 8, 15, 16, 23, 42 };
+// int total = 0;
+
+// foreach (int number in numbers)
+// {
+//     total += number;
+//     bool found = false;
+
+//     if (number == 42)
+//        found = true;
+
+//     if (found) 
+//         Console.WriteLine("Set contains 42");
+
+// }
+// Console.WriteLine($"Total: {total}");
+
+// string pangram = "The quick brown fox jumps over the lazy dog";
+// string[] words = pangram.Split(' ');
